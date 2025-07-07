9b9938e53e3a657df2b438b5981ad1acf5e0bc8c
Ellen <lidenellen@gmail.com>
2025-07-07 17:09:51 +0200

    Lagt till todo-formulär och lista med inmatningslogik

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,27 @@
+
+// Todo-appen
+const todoList = document.getElementById(
+  "todo-list"
+) as HTMLUListElement | null;
+const todoForm = document.getElementById("todo-form") as HTMLFormElement | null;
+const todoInput = document.getElementById(
+  "todo-input"
+) as HTMLInputElement | null;
+
+// Lyssna på när formuläret skickas in (tex när användaren trycker på "Lägg till")
+todoForm?.addEventListener("submit", (e) => {
+  e.preventDefault(); // Hindra att sidan laddas om
+
+  // Hämta/ta bort mellanslag från inmatningsfältet
+  const task = todoInput?.value.trim();
+
+  // Om fältet är tomt eller null, gör ingenting
+  if (todoInput?.value == "" || todoInput?.value == null) return
+ 
+})
+
+
+
 // Hamburger-menylogik
 const hamburger = document.querySelector(
   ".hamburger"
